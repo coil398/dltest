e1fd50f1bc0dfbaaf49c32adac0f507e3da0d1d4
coil398 <konayuta.sou@gmail.com>
2016-12-29 23:20:53 +0900

    modified the parameters.

--- model.py
+++ model.py
@@ -10,9 +10,10 @@ from keras.models import Sequential
 def build_deep_cnn(num_classes=3):
     model = Sequential()
 
-    model.add(Convolution2D(196, 3, 3, border_mode='same', input_shape=(1, 112, 112)))
+    model.add(Convolution2D(112, 4, 4, border_mode='same', input_shape=(112, 112, 1)))
     model.add(Activation('relu'))
 
+    """
     model.add(Convolution2D(256, 3, 3))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
@@ -25,6 +26,8 @@ def build_deep_cnn(num_classes=3):
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.5))
+
+"""
 
     model.add(Flatten())
     model.add(Dense(1024))
